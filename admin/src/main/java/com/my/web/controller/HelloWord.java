a932cb7f7f84c735025812d0941e891017cf41b2
494766750 <494766750@qq.com>
2022-02-21 10:57:55 +0800

    junit

--- admin/src/main/java/com/my/web/controller/HelloWord.java
+++ admin/src/main/java/com/my/web/controller/HelloWord.java
@@ -1,10 +1,14 @@
 package com.my.web.controller;
 
 import com.my.service.SysUserService;
+import my.upload.client.UploadClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/helloword")
@@ -25,5 +29,12 @@ public class HelloWord {
     @GetMapping("/deptTest")
     public Object deptTest(){
         return sysUserService.selectList();
+    }
+    
+    @GetMapping("/upload")
+    public Object upload(@RequestParam Map<String,String> map){
+//        new UploadClient("F:\\vx\\WeChat Files\\wxid_itxjadm1sbtn51\\FileStorage\\File\\2022-02\\CESOFT_XMLJ20211208.bak","127.0.0.1",8099).execute();
+//        new UploadClient(map.get("fileUrl"),map.get("ip"), Integer.parseInt(map.get("port"))).execute();
+        return "";
     }
 }
